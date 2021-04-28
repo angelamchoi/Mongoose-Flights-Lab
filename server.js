b3d6532486438551ebd298c72c5beda85d3f3f7f
Angela Choi <angelamchoi@gmail.com>
2021-04-27 23:16:32 -0400

    worked on part 2 and views

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+//connects with mongoose
 require('./config/database');
 
 var indexRouter = require('./routes/index');
